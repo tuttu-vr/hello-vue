a80d091ff2d383a9ce162d24290ca23b8c4175b1
tuttu-vr <tuttu74459297@gmail.com>
2020-07-07 03:30:13 +0900

    setup vue components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Example from './components/example.vue'
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        Example,
+    },
+    data: {
+        message: 'Hello World'
+    }
+})
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+}
